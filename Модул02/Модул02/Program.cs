eeee686058bdc7fe942384399ec920b289c4e478
pipetrov <pip9@abv.bg>
2016-01-20 18:02:23 +0200

    Модул 02. Видео 02 - Променливи от тип int

--- Модул02/Модул02/Program.cs
+++ Модул02/Модул02/Program.cs
@@ -6,7 +6,26 @@ namespace Модул02
 	{
 		public static void Main (string[] args)
 		{
-			Console.WriteLine ("Hello World!");
+			//Дефиниране на променливи
+			int a = 0,b = 0;
+			int sum = 0;
+
+			//Събиране на числа
+			a = 55;
+			b = 10;
+			sum = a + b;
+				
+
+			//Писане в конзола
+			Console.Write ( a );
+			Console.Write (" + ");
+			Console.Write ( b );
+			Console.Write (" = ");
+			Console.WriteLine (sum);
+
+
+
+
 		}
 	}
 }
