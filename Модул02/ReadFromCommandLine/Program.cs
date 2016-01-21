3decc443f3d60c0d89e894967047319d2ebc24eb
pipetrov <pip9@abv.bg>
2016-01-21 15:40:31 +0200

    Модул 02. Видео 06 - Работа със стрингове

--- Модул02/ReadFromCommandLine/Program.cs
+++ Модул02/ReadFromCommandLine/Program.cs
@@ -14,12 +14,20 @@ namespace ReadFromCommandLine
 			b = Convert.ToInt32 (Console.ReadLine ());
 
 			//Печатане на резултата в конзолата
-			Console.WriteLine ("Резултата от а + b е " + (a + b) + "\n\n" );
+			Console.WriteLine ("Резултата от а + b е " + (a + b) + "\n" );
+			string test = " 1";
+			test += ", 2";
+			test += ", 3";
+			test += ", 4";
+			test += ", 5";
+			test += ", 6";
+
+			Console.WriteLine ("Събиране с +=" + test + "\n");
+			test = test.Replace (", ","; ");
+			Console.WriteLine ("Работа с Replace :" + test + "\n");
+
+			Console.WriteLine ("Третия знак е:" + test.Split (';') [2]);
 
 
-
-
-			Console.WriteLine ("Hello World!");
-		}
 	}
-}
+	}}
